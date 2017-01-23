4e0e526714ad748518b1b2a835ae0e4ae3fd1129
Srikanth Kangyam <skangyam@9.109.222.200>
2017-01-23 17:12:37 +0530

    Added a variant

--- FarBeyond/src/AddDigitsChk.java
+++ FarBeyond/src/AddDigitsChk.java
@@ -5,9 +5,28 @@ public class AddDigitsChk {
 		System.out.println("AddDigits(38): " + addDigits(38));
 		System.out.println("AddDigits(123): " + addDigits(123));
 		System.out.println("AddDigits(79): " + addDigits(79));
+		System.out.println("AddDigits(1325132435356): " + addDigitsV(1325132435356L));
+		System.out.println("AddDigits(-3456): " + addDigitsV(-3456));
+		System.out.println("AddDigits(10): " + addDigitsV(10));
 		
 		
 
+	}
+	
+	public static int addDigitsV(long num) {
+		if (num / 10 == 0) {
+			return (int) num;
+		}
+		if (num < 0) {
+			num = Math.abs(num);
+		}
+		long quo = 0;
+		long rem = 0;
+		while (num > 0) {
+			rem += num % 10;
+			num = num / 10;
+		}
+		return (int) rem;
 	}
 	
     public static int addDigits(int num) {
